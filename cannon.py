09253e72043965f6c947459dd093c940ade7b9af
Samperio11 <a01662935@tec.mx>
2023-03-23 13:46:28 -0600

    Aumente la velocidad horizontal y reduje la parábola de la pelota

--- cannon.py
+++ cannon.py
@@ -55,10 +55,10 @@ def move():
         targets.append(target)
 
     for target in targets:
-        target.x -= 0.5
+        target.x -= 6
 
     if inside(ball):
-        speed.y -= 0.35
+        speed.y -= 0.7
         ball.move(speed)
 
     dupe = targets.copy()
@@ -74,7 +74,7 @@ def move():
         if not inside(target):
             return
 
-    ontimer(move, 50)
+    ontimer(move, 20)
 
 
 setup(420, 420, 370, 0)
