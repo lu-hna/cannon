2cde4f62bebce54bd4c987508c3633165fe1bd57
Samperio11 <a01662935@tec.mx>
2023-03-23 13:33:14 -0600

    Se cambia el divisor de la linea 26 y 27, de 25 a 9 para que se muevan rapido

--- cannon.py
+++ cannon.py
@@ -23,8 +23,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (x + 200) / 9
+        speed.y = (y + 200) / 9
 
 
 def inside(xy):
